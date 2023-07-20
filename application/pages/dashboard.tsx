3310e66eabd2a9970656f31d685c3e78f49fa890
Raziur306 <raziur.rahaman.ronju@gmail.com>
2023-07-21 01:35:28 +0600

    dashboard upcoming added

--- application/pages/dashboard.tsx
+++ application/pages/dashboard.tsx
@@ -12,15 +12,8 @@ const Dashboard = () => {
     return (
         <>
             <Grid sx={{ color: 'white', pt: 10, gap: 20 }} container justifyContent={"center"} >
-                <Grid item xs={5} justifyContent={'center'}>
-                    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 5 }}>
-                        <Typography sx={{ textAlign: 'center', fontWeight: 'bold' }} variant='h3' >Ongoing Elections</Typography>
-                        <UpcomingElectionCard />
-                        <UpcomingElectionCard />
-                        <UpcomingElectionCard />
-                    </Box>
-                </Grid>
-                <Grid item xs={5} justifyContent={'center'}>
+
+                <Grid item xs={3} justifyContent={'center'}>
                     <Box sx={{ display: 'flex', flexDirection: 'column', gap: 5 }}>
                         <Typography sx={{ textAlign: 'center', fontWeight: 'bold' }} variant='h3' fontWeight={'bold'}>Previous Elections</Typography>
                         {
@@ -31,6 +24,29 @@ const Dashboard = () => {
                         }
                     </Box>
                 </Grid>
+
+
+                <Grid item xs={3} justifyContent={'center'}>
+                    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 5 }}>
+                        <Typography sx={{ textAlign: 'center', fontWeight: 'bold' }} variant='h3' fontWeight={'bold'}>Ongoing Election</Typography>
+                        {
+                            previousElection?.map((election: any, index: any) => {
+                                const { name, hash, startTime, endTime } = election;
+                                return <PreviousElectionCard electionHash={hash} electionName={name} startTime={startTime} endTime={endTime} key={index} />
+                            })
+                        }
+                    </Box>
+                </Grid>
+
+                <Grid item xs={3} justifyContent={'center'}>
+                    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 5 }}>
+                        <Typography sx={{ textAlign: 'center', fontWeight: 'bold' }} variant='h3' >Upcoming Elections</Typography>
+                        <UpcomingElectionCard />
+                        <UpcomingElectionCard />
+                        <UpcomingElectionCard />
+                    </Box>
+                </Grid>
+
 
             </Grid>
         </>)
